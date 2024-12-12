53521b3444b0bcb4f6b82c6d258886ce697f0c34
Facundo <facund097dv@gmail.com>
2024-12-12 09:32:11 -0300

    4.4

--- index.js
+++ index.js
@@ -1,43 +1,7 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-});
+const app = require("./app");
+const config = require("./utils/config");
 
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = process.env.MONGODB_URI;
-mongoose
-    .connect(mongoUrl)
-    .then(() => console.log("Connecting to MongoDB..."))
-    .catch((error) =>
-        console.error(`Error to connecting to database: ${error}`)
-    );
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs);
-    });
-});
-
-app.post("/api/blogs", (request, response) => {
-    const blog = new Blog(request.body);
-
-    blog.save().then((result) => {
-        response.status(201).json(result);
-    });
-});
-
-const PORT = process.env.PORT;
+const PORT = config.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
---
+++ utils/list_helper.js
@@ -0,0 +1,14 @@
+const dummy = (blogs) => {
+    return 1;
+};
+
+const totalLikes = (blogs) => {
+    let likes = 0;
+    blogs.map((blog) => {
+        likes += blog.likes;
+    });
+
+    return likes;
+};
+
+module.exports = { dummy, totalLikes };
